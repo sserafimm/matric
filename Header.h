b1326b37b1a6c819e9f0c13d7aa84c73112913d0
sserafimm <75272572+sserafimm@users.noreply.github.com>
2021-06-21 23:30:54 +0300

    Add files via upload

--- Header.h
+++ Header.h
@@ -1,235 +1,180 @@
 #pragma once
-#include "Header.h"
-#include  <vector>
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <algorithm>
+#include <exception>
 
-namespace Project2 {
+using std::vector;
 
-	using namespace System;
-	using namespace System::ComponentModel;
-	using namespace System::Collections;
-	using namespace System::Windows::Forms;
-	using namespace System::Data;
-	using namespace System::Drawing;
-	using std::vector;
-
-	/// <summary>
-	/// Сводка для MyForm
-	/// </summary>
-	public ref class MyForm : public System::Windows::Forms::Form
-	{
-	public:
-		MyForm(void)
-		{
-			InitializeComponent();
-			//
-			//TODO: добавьте код конструктора
-			//
-		}
-
-	protected:
-		/// <summary>
-		/// Освободить все используемые ресурсы.
-		/// </summary>
-		~MyForm()
-		{
-			if (components)
-			{
-				delete components;
-			}
-		}
-	private: System::Windows::Forms::DataGridView^ dataGridView1;
-	protected:
-	private: System::Windows::Forms::NumericUpDown^ numericUpDown1;
-	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::Button^ button2;
-	private: System::Windows::Forms::Label^ label1;
-	private: System::Windows::Forms::Label^ label2;
-
-
-	private:
-		/// <summary>
-		/// Обязательная переменная конструктора.
-		/// </summary>
-		System::ComponentModel::Container^ components;
-
-#pragma region Windows Form Designer generated code
-		/// <summary>
-		/// Требуемый метод для поддержки конструктора — не изменяйте 
-		/// содержимое этого метода с помощью редактора кода.
-		/// </summary>
-		void InitializeComponent(void)
-		{
-			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
-			this->numericUpDown1 = (gcnew System::Windows::Forms::NumericUpDown());
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->button2 = (gcnew System::Windows::Forms::Button());
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->label2 = (gcnew System::Windows::Forms::Label());
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->BeginInit();
-			this->SuspendLayout();
-			// 
-			// dataGridView1
-			// 
-			this->dataGridView1->AllowUserToAddRows = false;
-			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView1->Location = System::Drawing::Point(12, 26);
-			this->dataGridView1->Name = L"dataGridView1";
-			this->dataGridView1->Size = System::Drawing::Size(572, 321);
-			this->dataGridView1->TabIndex = 0;
-			this->dataGridView1->CellContentClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &MyForm::dataGridView1_CellContentClick);
-			// 
-			// numericUpDown1
-			// 
-			this->numericUpDown1->Location = System::Drawing::Point(590, 26);
-			this->numericUpDown1->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 10, 0, 0, 0 });
-			this->numericUpDown1->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1, 0, 0, 0 });
-			this->numericUpDown1->Name = L"numericUpDown1";
-			this->numericUpDown1->Size = System::Drawing::Size(120, 20);
-			this->numericUpDown1->TabIndex = 1;
-			this->numericUpDown1->Value = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1, 0, 0, 0 });
-			this->numericUpDown1->ValueChanged += gcnew System::EventHandler(this, &MyForm::numericUpDown1_ValueChanged);
-			// 
-			// button1
-			// 
-			this->button1->Font = (gcnew System::Drawing::Font(L"Javanese Text", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->button1->Location = System::Drawing::Point(25, 362);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(338, 71);
-			this->button1->TabIndex = 2;
-			this->button1->Text = L"Сгенерировать матрицу";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
-			// 
-			// button2
-			// 
-			this->button2->Location = System::Drawing::Point(590, 63);
-			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(118, 23);
-			this->button2->TabIndex = 3;
-			this->button2->Text = L"button2";
-			this->button2->UseVisualStyleBackColor = true;
-			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
-			// 
-			// label1
-			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(591, 123);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(35, 13);
-			this->label1->TabIndex = 4;
-			this->label1->Text = L"label1";
-			// 
-			// label2
-			// 
-			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(726, 123);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(35, 13);
-			this->label2->TabIndex = 5;
-			this->label2->Text = L"label2";
-			// 
-			// MyForm
-			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
-			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(835, 496);
-			this->Controls->Add(this->label2);
-			this->Controls->Add(this->label1);
-			this->Controls->Add(this->button2);
-			this->Controls->Add(this->button1);
-			this->Controls->Add(this->numericUpDown1);
-			this->Controls->Add(this->dataGridView1);
-			this->Name = L"MyForm";
-			this->Text = L"MyForm";
-			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->EndInit();
-			this->ResumeLayout(false);
-			this->PerformLayout();
-
-		}
-#pragma endregion
-	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
-	}
-	private: System::Void numericUpDown1_ValueChanged(System::Object^ sender, System::EventArgs^ e) {
-		int number = Convert::ToInt32(numericUpDown1->Value);
-
-		dataGridView1->Columns->Clear();
-		for (int i = 0; i < number; i++)
-		{
-			dataGridView1->Columns->Add(i.ToString(), "x" + (i + 1).ToString());
-		}
-		dataGridView1->Columns->Add(number.ToString(), "y");
-		for (int i = 0; i < number; i++)
-		{
-			dataGridView1->Rows->Add();
-		}
-	}
-	private: System::Void dataGridView1_CellContentClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
-	}
-	private: System::Void backgroundWorker1_DoWork(System::Object^ sender, System::ComponentModel::DoWorkEventArgs^ e) {
-	}
-	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-		dataGridView1->RowCount = 5;
-		dataGridView1->ColumnCount = 5;
-
-		dataGridView1->TopLeftHeaderCell->Value = "Матрица";
-		for (int i = 5; i < 5; i++)
-		{
-			for (int j = 0; j < 5; j++)
-
-			{
-
-				dataGridView1->Rows[i]->Cells[j]->Value = (i * j).ToString();
-			}
-			dataGridView1->Rows[i]->HeaderCell->Value = System::Convert::ToString(i + 1);
-			dataGridView1->Columns[i]->HeaderCell->Value = System::Convert::ToString(i + 1);
-		}
-	}
-	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
-		vector<vector<System::Double>> vec;
-		auto rows = dataGridView1->Rows->Count;
-		auto cols = dataGridView1->Columns->Count;
-
-		vec.resize(rows);
-
-		for (size_t i = 0; i < rows; i++)
-		{
-			vec[i].resize(cols);
-			for (size_t j = 0; j < cols; j++)
-			{
-				vec[i][j] = Convert::ToDouble(dataGridView1->Rows[i]->Cells[j]->Value);
-			}
-		}
-
-
-		if (rows <= 0)
-		{
-			MessageBox::Show("Size cannot lower or equals to zero");
-		}
-		else
-		{
-			//show_matrix(matrix);
-			label1->Text = "";
-			vector<double> x = findMaximumRowNums(vec);
-			for (int i = 0; i < x.size(); i++)
-			{
-				label1->Text += "x" + (i + 1).ToString() + "=" + x[i].ToString() + "\n";
-			}
-			//printAbsoluteVector(x);
-			label2->Text = "";
-			vector<double> y = ZeydelPivoting(vec);
-			for (int i = 0; i < y.size(); i++)
-			{
-				label2->Text += "x" + (i + 1).ToString() + "=" + y[i].ToString() + "\n";
-			}
-			//printScaleVector(y);
-		}
-
-
-		vec.clear();
-	}
-	};
+void printAbsoluteVector(std::vector<double> abs_vector)
+{
+    std::cout << "The absolute coefficients are: [";
+    for (int i = 0; i < abs_vector.size(); ++i)
+        std::cout << abs_vector[i] << " ";
+    std::cout << "]" << std::endl;
 }
+
+void printScaleVector(std::vector<double> abs_vector)
+{
+    std::cout << "The scaling coefficients are: [";
+    for (int i = 0; i < abs_vector.size(); ++i)
+        std::cout << abs_vector[i] << " ";
+    std::cout << "]" << std::endl;
+}
+
+std::vector<double> findMaximumRowNums(std::vector<std::vector<double>> augmented_matrix)
+{
+    std::vector<double> maximums;
+    for (int i = 0; i < augmented_matrix.size(); ++i)
+    {
+        double maxel = augmented_matrix[i][0];
+        for (size_t j = 1; j < augmented_matrix[i].size(); j++)
+        {
+            maxel = fmax(maxel, augmented_matrix[i][j]);
+        }
+
+        //std::max(augmented_matrix[i].begin(), augmented_matrix[i].end());
+
+
+        maximums.push_back(maxel);
+    }
+    return maximums;
+}
+
+std::vector<double> makeScaleVector(std::vector<std::vector<double>>augmented_matrix, std::vector<double>abs_coefficients, int i)
+{
+    std::vector<double> scale_vector;
+
+
+    for (int j = i; j < augmented_matrix.size(); ++j)
+        scale_vector.push_back(abs(augmented_matrix[j][i] / abs_coefficients[j]));
+    return scale_vector;
+}
+
+std::vector<double> ZeydelPivoting(std::vector<std::vector<double>> augmented_matrix)
+{
+    int equations_num = augmented_matrix.size();
+    std::vector<double> abs_coefficients = findMaximumRowNums(augmented_matrix);
+    printAbsoluteVector(abs_coefficients);
+
+    std::vector<double> scale_vector;
+    for (int i = 0; i < equations_num; ++i)
+    {
+        scale_vector = makeScaleVector(augmented_matrix, abs_coefficients, i);
+        printScaleVector(scale_vector);
+    }
+    return scale_vector;
+}
+
+void show_matrix(std::vector<std::vector<double>> matr)
+{
+    std::cout << "Matrix \n";
+    for (size_t i = 0; i < matr.size(); i++)
+    {
+        for (size_t j = 0; j < matr[i].size() - 1; j++)
+        {
+            std::cout << matr[i][j] << "\t";
+        }
+        std::cout << "|\t" << matr[i][matr[i].size() - 1] << "\n";
+    }
+    std::cout << "\n";
+}
+
+
+//void printAbsoluteVector(std::vector<double> abs_vector)
+//{
+//    std::cout << "The absolute coefficients are: [";
+//    for (int i = 0; i < abs_vector.size(); ++i)
+//        std::cout << abs_vector[i] << " ";
+//    std::cout << "]" << std::endl;
+//}
+//
+//void printScaleVector(std::vector<double> abs_vector)
+//{
+//    std::cout << "The scaling coefficients are: [";
+//    for (int i = 0; i < abs_vector.size(); ++i)
+//        std::cout << abs_vector[i] << " ";
+//    std::cout << "]" << std::endl;
+//}
+//
+//std::vector<double> findMaximumRowNums(std::vector<std::vector<double>> augmented_matrix)
+//{
+//    std::vector<double> maximums;
+//    for (int i = 0; i < augmented_matrix.size(); ++i)
+//    {
+//        double max = *std::max_element(augmented_matrix[i].begin(), augmented_matrix[i].end() - 1);
+//        maximums.push_back(max);
+//    }
+//    return maximums;
+//}
+//
+//std::vector<double> makeScaleVector(std::vector<std::vector<double>>augmented_matrix, std::vector<double>abs_coefficients, int i)
+//{
+//    std::vector<double> scale_vector;
+//
+//
+//    for (int j = i; j < augmented_matrix.size(); ++j)
+//        scale_vector.push_back(abs(augmented_matrix[j][i] / abs_coefficients[j]));
+//    return scale_vector;
+//}
+//
+//
+////solves given augmented matrix by Gaussian elimination with scaled partial pivoting
+//
+//std::vector<double> gaussPivoting(std::vector<std::vector<double>> augmented_matrix)
+//{
+//    int equations_num = augmented_matrix.size(); //number of equations
+//    std::vector<double> abs_coefficients = findMaximumRowNums(augmented_matrix);
+//    printAbsoluteVector(abs_coefficients);
+//
+//    //i is the iterator pointing a column
+//    for (int i = 0; i < equations_num; ++i)
+//    {
+//        std::vector<double> scale_vector = makeScaleVector(augmented_matrix, abs_coefficients, i);
+//        printScaleVector(scale_vector);
+//
+//        //find maximum absolute coefficients in each row
+//        int max_rows_num = i;
+//        for (int j = i + 1; j < equations_num; ++j)
+//        {
+//            if (abs(augmented_matrix[j][i]) > abs(augmented_matrix[max_rows_num][i]))
+//            {
+//                max_rows_num = j;
+//            }
+//        }
+//
+//        //swap current row with max absolute pivot column coeff row
+//        swap(augmented_matrix[i], augmented_matrix[max_rows_num]);
+//
+//        for (int j = i + 1; j < equations_num; ++j)
+//            for (int k = i; k <= equations_num; ++k)
+//            {
+//                augmented_matrix[j][k] -= augmented_matrix[j][i] * augmented_matrix[i][k] / augmented_matrix[i][i];
+//                std::cout << augmented_matrix[j][k] << " + " << augmented_matrix[j][i] << " * " << augmented_matrix[i][k] << " / " << augmented_matrix[i][i] << "\n";
+//            }
+//
+//        //printEquations(augmented_matrix);
+//        printEquations(augmented_matrix);
+//        scale_vector.clear();
+//
+//    }
+//
+//
+//    //performing backward substitution
+//    std::vector<double> solution(equations_num);
+//
+//    for (int i = equations_num - 1; i >= 0; --i)
+//    {
+//        solution[i] = augmented_matrix[i][equations_num] / augmented_matrix[i][i];
+//        //substitute immediate solution backward and update RHS of each equation
+//        for (int j = i - 1; j >= 0; --j)
+//        {
+//            augmented_matrix[j][equations_num] -= augmented_matrix[j][i] * solution[i];
+//
+//        }
+//
+//    }
+//    printEquations(augmented_matrix);
+//    return solution;
+//}
--- MyForm.h
+++ MyForm.h
@@ -40,8 +40,14 @@ namespace Project2 {
 	private: System::Windows::Forms::DataGridView^ dataGridView1;
 	protected:
 	private: System::Windows::Forms::NumericUpDown^ numericUpDown1;
-	private: System::Windows::Forms::Button^ button1;
+
 	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::MenuStrip^ menuStrip1;
+	private: System::Windows::Forms::ToolStripMenuItem^ менюToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ справкаToolStripMenuItem;
+	private: System::Windows::Forms::Label^ label3;
 
 
 	private:
@@ -59,25 +65,34 @@ namespace Project2 {
 		{
 			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
 			this->numericUpDown1 = (gcnew System::Windows::Forms::NumericUpDown());
-			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->менюToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->справкаToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->label3 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->BeginInit();
+			this->menuStrip1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// dataGridView1
 			// 
 			this->dataGridView1->AllowUserToAddRows = false;
+			this->dataGridView1->AutoSizeColumnsMode = System::Windows::Forms::DataGridViewAutoSizeColumnsMode::AllCells;
+			this->dataGridView1->AutoSizeRowsMode = System::Windows::Forms::DataGridViewAutoSizeRowsMode::AllCells;
 			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView1->Location = System::Drawing::Point(12, 26);
+			this->dataGridView1->Location = System::Drawing::Point(17, 27);
 			this->dataGridView1->Name = L"dataGridView1";
-			this->dataGridView1->Size = System::Drawing::Size(572, 321);
+			this->dataGridView1->RowHeadersWidth = 20;
+			this->dataGridView1->Size = System::Drawing::Size(605, 289);
 			this->dataGridView1->TabIndex = 0;
 			this->dataGridView1->CellContentClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &MyForm::dataGridView1_CellContentClick);
 			// 
 			// numericUpDown1
 			// 
-			this->numericUpDown1->Location = System::Drawing::Point(692, 309);
+			this->numericUpDown1->Location = System::Drawing::Point(650, 46);
 			this->numericUpDown1->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 10, 0, 0, 0 });
 			this->numericUpDown1->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1, 0, 0, 0 });
 			this->numericUpDown1->Name = L"numericUpDown1";
@@ -86,43 +101,91 @@ namespace Project2 {
 			this->numericUpDown1->Value = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1, 0, 0, 0 });
 			this->numericUpDown1->ValueChanged += gcnew System::EventHandler(this, &MyForm::numericUpDown1_ValueChanged);
 			// 
-			// button1
-			// 
-			this->button1->Font = (gcnew System::Drawing::Font(L"Javanese Text", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->button1->Location = System::Drawing::Point(169, 389);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(338, 71);
-			this->button1->TabIndex = 2;
-			this->button1->Text = L"Сгенерировать матрицу";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
-			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(555, 410);
+			this->button2->Location = System::Drawing::Point(650, 103);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(118, 23);
 			this->button2->TabIndex = 3;
-			this->button2->Text = L"button2";
+			this->button2->Text = L"Решить";
 			this->button2->UseVisualStyleBackColor = true;
 			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(647, 174);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(109, 13);
+			this->label1->TabIndex = 4;
+			this->label1->Text = L"Абсолютный вектор";
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(647, 205);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(102, 13);
+			this->label2->TabIndex = 5;
+			this->label2->Text = L"Скалярный вектор";
+			this->label2->Click += gcnew System::EventHandler(this, &MyForm::label2_Click);
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->менюToolStripMenuItem });
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(835, 24);
+			this->menuStrip1->TabIndex = 6;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// менюToolStripMenuItem
+			// 
+			this->менюToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->справкаToolStripMenuItem });
+			this->менюToolStripMenuItem->Name = L"менюToolStripMenuItem";
+			this->менюToolStripMenuItem->Size = System::Drawing::Size(53, 20);
+			this->менюToolStripMenuItem->Text = L"Меню";
+			this->менюToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::менюToolStripMenuItem_Click);
+			// 
+			// справкаToolStripMenuItem
+			// 
+			this->справкаToolStripMenuItem->Name = L"справкаToolStripMenuItem";
+			this->справкаToolStripMenuItem->Size = System::Drawing::Size(180, 22);
+			this->справкаToolStripMenuItem->Text = L"справка";
+			this->справкаToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::справкаToolStripMenuItem_Click);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(647, 28);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(123, 13);
+			this->label3->TabIndex = 7;
+			this->label3->Text = L"Размерность матрицы";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(835, 496);
+			this->ClientSize = System::Drawing::Size(835, 327);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->button2);
-			this->Controls->Add(this->button1);
 			this->Controls->Add(this->numericUpDown1);
 			this->Controls->Add(this->dataGridView1);
+			this->Controls->Add(this->menuStrip1);
+			this->MainMenuStrip = this->menuStrip1;
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->EndInit();
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
@@ -132,10 +195,11 @@ namespace Project2 {
 		int number = Convert::ToInt32(numericUpDown1->Value);
 
 		dataGridView1->Columns->Clear();
-		for (int i = 0; i <= number; i++)
+		for (int i = 0; i < number; i++)
 		{
-			dataGridView1->Columns->Add(i.ToString(), i.ToString() + 1);
+			dataGridView1->Columns->Add(i.ToString(), "x" + (i + 1).ToString());
 		}
+		dataGridView1->Columns->Add(number.ToString(), "y");
 		for (int i = 0; i < number; i++)
 		{
 			dataGridView1->Rows->Add();
@@ -162,25 +226,57 @@ namespace Project2 {
 			dataGridView1->Columns[i]->HeaderCell->Value = System::Convert::ToString(i + 1);
 		}
 	}
-private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
-	vector<vector<System::Double>> vec;
-	auto rows = dataGridView1->Rows->Count;
-	auto cols = dataGridView1->Columns->Count;
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		vector<vector<System::Double>> vec;
+		auto rows = dataGridView1->Rows->Count;
+		auto cols = dataGridView1->Columns->Count;
 
-	vec.resize(rows);
+		vec.resize(rows);
 
-	for (size_t i = 0; i < rows; i++)
-	{
-		vec[i].resize(cols);
-		for (size_t j = 0; j < cols; j++)
+		for (size_t i = 0; i < rows; i++)
 		{
-			vec[i][j] = Convert::ToDouble(dataGridView1->Rows[i]->Cells[j]->Value);
+			vec[i].resize(cols);
+			for (size_t j = 0; j < cols; j++)
+			{
+				vec[i][j] = Convert::ToDouble(dataGridView1->Rows[i]->Cells[j]->Value);
+			}
 		}
+
+
+		if (rows <= 0)
+		{
+			MessageBox::Show("Size cannot lower or equals to zero");
+		}
+		else
+		{
+			//show_matrix(matrix);
+			label1->Text = "";
+			vector<double> x = findMaximumRowNums(vec);
+			for (int i = 0; i < x.size(); i++)
+			{
+				label1->Text += "x" + (i + 1).ToString() + "=" + x[i].ToString() + "\n";
+			}
+			//printAbsoluteVector(x);
+			label2->Text = "";
+			vector<double> y = ZeydelPivoting(vec);
+			for (int i = 0; i < y.size(); i++)
+			{
+				label2->Text += "x" + (i + 1).ToString() + "=" + y[i].ToString() + "\n";
+			}
+			//printScaleVector(y);
+		}
+
+
+		vec.clear();
 	}
-
-	show_matrix(vec);
-	gaussPivoting(vec);
-
+	private: System::Void справкаToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
+		MessageBox::Show("Для того чтобы создать систему линейных алгебраических уравнений, необходимо задать количество неизвестных для расширенной матрицы системы линейных алгебраичных уравнений. Необходимо данную таблицу заполнить действительными числами. Для того чтобы расчитать иатрицу необходимо нажать на кнопку\"Решить\"расположенной под кнопкой размерности матрицы    ");
+	}
+private: System::Void менюToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
 }
 };
 }
